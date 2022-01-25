e2de15edbc3154d063338e634afb15f41ab2def2
milad mdoaresi <miladmodaresi525@gmial.com>
2022-01-25 22:40:35 +0330

    feature: users

---
+++ src/users/user.schema.ts
@@ -0,0 +1,21 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { Document } from 'mongoose';
+
+export type UserDocument = User & Document;
+
+@Schema()
+export class User {
+  @Prop({ required: true })
+  first_name: string;
+
+  @Prop({ required: true })
+  last_name: string;
+
+  @Prop({ required: true })
+  is_employed: boolean;
+
+  @Prop()
+  description: string;
+}
+
+export const UserSchema = SchemaFactory.createForClass(User);
---
+++ src/users/users.controller.ts
@@ -0,0 +1,29 @@
+import { Body, Controller, Delete, Get, HttpStatus, Param, Post, Res } from '@nestjs/common';
+import { CreateUserDto } from './dto/create-user.dto';
+import { Response } from 'express';
+import { UsersService } from './users.service';
+import { User } from './user.schema';
+
+@Controller('users')
+export class UsersController {
+    constructor(private usersService: UsersService) {}
+
+    @Post()
+    create(@Body() createuserDto: CreateUserDto,@Res() res: Response) {
+        this.usersService.create(createuserDto);
+        res.status(HttpStatus.CREATED).json();
+      
+    }
+  
+    @Get()
+    async findAll(@Res() res: Response) {
+        res.status(HttpStatus.OK).json(await this.usersService.findAll());
+
+    }
+
+    @Delete(':id')
+    remove(@Param('id') id: string) {
+      return `This action removes a #${id} cat`;
+    }
+  
+}
